e3be547a8577bcaf6320ac8253e2c502c4d992b4
dhia-eddine <diamandouj@gmail.com>
2024-04-23 20:03:01 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-
-import './App.css'
-import LoginPage from './app/paages/login/LoginPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import LoginPage from './app/pages/login/LoginPage';
+import Navbar from './app/pages/nav/Navbar';
 
 function App() {
-
   return (
-    <LoginPage />
-  )
+    <BrowserRouter>
+      <Routes> {/* Wrap your routes with <Routes> */}
+        <Route path="/login" element={<LoginPage />} /> {/* Use the "element" prop instead of "Component" */}
+        <Route path="/" element={<Navbar />} /> {/* Use the "element" prop instead of "Component" */}
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
-export default App
+
+export default App;
